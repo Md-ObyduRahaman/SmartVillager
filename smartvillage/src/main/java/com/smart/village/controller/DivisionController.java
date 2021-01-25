d52d1b059ae93522ed5bc05e132b4788df07c803
DCL <DCL@192.168.0.105>
2021-01-25 14:42:20 +0600

    UserSms html form configure

--- smartvillage/src/main/java/com/smart/village/controller/DivisionController.java
+++ smartvillage/src/main/java/com/smart/village/controller/DivisionController.java
@@ -1,7 +1,10 @@
 package com.smart.village.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.smart.village.model.UserSms;
 
 @Controller
 public class DivisionController {
@@ -9,11 +12,12 @@ public class DivisionController {
 	@GetMapping("/mymensingh")
 	public String mymensingh() {
 		System.out.println("mymensingh controller work");
-
 		return "mymensingh_division";
 	}
 	@GetMapping("/contact")
-	public String contact() {
+	public String contact (Model model) {
+		UserSms userSms=new UserSms();
+		model.addAttribute("userSms",userSms);
 		System.out.println("contact");
 		return "contact";
 		
--- smartvillage/src/main/java/com/smart/village/controller/HomeController.java
+++ smartvillage/src/main/java/com/smart/village/controller/HomeController.java
@@ -1,16 +1,34 @@
 package com.smart.village.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.smart.village.model.UserSms;
 
 @Controller
 public class HomeController {
+	
+	/*
+	 * @Autowired(required=true) UserSms userSms;
+	 */
 	
 	@GetMapping("/index")
 	public String index() {
 		System.out.println("controller work");
 		return "index";
+	}
+	@RequestMapping(value = "/submit", method = RequestMethod.POST)
+	public String submit(@ModelAttribute("userSms") UserSms userSms, BindingResult result)
+	{
+		System.out.println("submit is worked");
+		System.out.println(userSms.toString());
+		return "contact-success"; 
 	}
 
 }
